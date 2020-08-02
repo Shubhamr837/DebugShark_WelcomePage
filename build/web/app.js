1948e23c6ae9d60a1b0658933ac72993b9dde2cb
Subham raj <45128519+Shubhamr837@users.noreply.github.com>
2020-08-02 15:58:42 +0530

    added builds

---
+++ build/web/app.js
@@ -0,0 +1,8 @@
+function openRegistrationPage(text) {
+    console.log(text)
+    window.location.href = '/registration.html';
+}
+function openLoginPage(text) {
+    console.log(text)
+    window.location.href = '/login.html';
+}
